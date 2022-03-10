7c093c486c6ec5e2ffda601c66d8f91aeb0b14b8
Fran.Maccio <franmaccio890@gmail.com>
2022-03-10 16:32:29 -0300

    obteniendo Token, incorporando redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import SpotifyWebApi from 'spotify-web-api-js';
 import Login from './components/Login/Login';
+import { getTokenFromURL } from './components/Login/LoginLogic';
 import Player from './components/Player/Player';
+import { SET_USER } from './features/counter/UserSlice';
+
+const spotify = new SpotifyWebApi();
+
 
 function App() {
+  const [token, setToken] = useState("")
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    const hash = getTokenFromURL();
+    window.location.hash ="";
+    const _token = hash.access_token;
+    dispatch(SET_USER(_token))
+    console.log("token =>", token);
+  }, [token])
   return (
-    <div className="App">
-      <Player />
-     {/* <Login /> */} 
+    <div >
+      {
+        token ?  <Player /> : <Login />
+      }   
     </div>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,10 @@
 import React from 'react'
+import { loginURL } from './LoginLogic'
 import { LoginButton, LoginContainer } from './styles'
-
-const endpoint = "https://accounts.spotify.com/authorize";
-const clientID = `270b420c3a4b442fbe44cf723c0c26d9`
-console.log = clientID;
-const redirectUri = "https://localhost:3000/"
-const scopes = ["user-read.currently-playing",
-"user-read.recently-played",
-"user-read.playback-state",
-"user-top-read",
-"user-modify-playback-state",]
-const loginURL = `${endpoint}?client_id=${clientID}&response_type=token&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&show_dialog=true}`
 
 
 const Login = () => {
+   
   return (
     <LoginContainer>
         <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png" alt="logo" className="logo" />
